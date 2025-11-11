34d451a215ebe2b712f0665a23b78ce2f34c03c6
Eckes_J\Eckes <363379340@qq.com>
2025-11-11 11:14:21 +0800

    no message

--- src/eckes/python/notify.py
+++ src/eckes/python/notify.py
@@ -59,7 +59,7 @@ push_config = {
 
     'PUSH_KEY': '',                     # server 酱的 PUSH_KEY，兼容旧版与 Turbo 版
 
-    'PUSH_PLUS_TOKEN': '',              # push+ 微信推送的用户令牌
+    'PUSH_PLUS_TOKEN': 'a1dfe7a5c3b94a82a4f8e51dd874dbce',              # push+ 微信推送的用户令牌
     'PUSH_PLUS_USER': '',               # push+ 微信推送的群组编码
 
     'QMSG_KEY': '',                     # qmsg 酱的 QMSG_KEY
@@ -80,10 +80,10 @@ notify_function = []
 # fmt: on
 
 # 首先读取 面板变量 或者 github action 运行变量
-for k in push_config:
-    if os.getenv(k):
-        v = os.getenv(k)
-        push_config[k] = v
+# for k in push_config:
+#     if os.getenv(k):
+#         v = os.getenv(k)
+#         push_config[k] = v
 
 
 def bark(title: str, content: str) -> None:
