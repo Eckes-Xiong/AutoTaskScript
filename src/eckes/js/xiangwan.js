a5ad729af2a4b35710e62544e2e743ea9a934f59
Eckes_J\Eckes <363379340@qq.com>
2025-11-19 17:23:40 +0800

    no message

--- src/eckes/js/xiangwan.js
+++ src/eckes/js/xiangwan.js
@@ -36,8 +36,8 @@ import { fileURLToPath } from 'url';
 const $ = {isLogin:false, name: 'APP'};
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// const xiangWanList = ["18908687553#ss123456","17786601235#a277141160","18963439553#ss123456"];
-const xiangWanList = ["18963439553#ss123456"];
+const xiangWanList = ["18908687553#ss123456","17786601235#a277141160","18963439553#ss123456"];
+// const xiangWanList = ["18963439553#ss123456"];
 // 消息推送
 let message = '';
 // token 缓存地址
